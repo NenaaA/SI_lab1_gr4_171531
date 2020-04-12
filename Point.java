2fa6709464b5e094f8f4013c747818c22985398f
NenaaA <48931420+NenaaA@users.noreply.github.com>
2020-04-12 14:23:39 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -8,13 +8,55 @@ class Point {
 
 	//TODO setters and getters
 
-	public void move (char xDirection, char yDirection) {
-		//TODO
-	}
+	public Point(String id,double x, double y, String color){
+        this.id = id;
+        this.x=x;
+        this.y=y;
+        this.color=color;
+    }
+    //TODO setters and getters
+    public String getId() {
+        return id;
+    }
 
-	public void draw () {
-		//TODO
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void move (char xDirection, char yDirection) {
+        //TODO
+        this.x = this.x + Double.parseDouble(String.valueOf(xDirection));
+        this.y = this.y + Double.parseDouble(String.valueOf(yDirection));
+    }
+
+    public void draw () {
+        //TODO
+        System.out.println("Current co-ordinations :" + "("+this.x + "," +this.y+")");
+    }
 
 
 
